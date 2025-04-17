febaeb0646bc6a29442ae1602b105df4e67d3c16
Alex Kling <momiji.kling@gmail.com>
2025-04-17 15:18:43 +0200

    finished c_01 ex02

---
+++ C_01/ex02/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+int	main() {
+
+	std::string		str = "HI THIS IS BRAIN";
+	std::string*	p_str = &str;
+	std::string		&r_str = str;
+
+	std::cout << "Printing the address of the string : ";
+	std::cout << &str << std::endl;
+
+	std::cout << "Printing the address of the pointer on a string : ";
+	std::cout << &p_str << std::endl;
+
+	std::cout << "Printing the address of the reference on a string : ";
+	std::cout << &r_str << std::endl;
+
+
+	std::cout << "Printing the value of the string : ";
+	std::cout << str << std::endl;
+
+	std::cout << "Printing the value of the pointer on a string : ";
+	std::cout << *p_str << std::endl;
+
+	std::cout << "Printing the value of the reference on a string : ";
+	std::cout << r_str << std::endl;
+
+}
+
+
+
