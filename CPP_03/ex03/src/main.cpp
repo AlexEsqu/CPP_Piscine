ea978754771b281637537192aafde444797ac6df
Alex <momiji.kling@gmail.com>
2025-04-25 12:16:30 +0200

    virtual tests for Diamond

--- CPP_03/ex03/src/main.cpp
+++ CPP_03/ex03/src/main.cpp
@@ -22,4 +22,25 @@ int	main()
 
 	}
 
+	ClapTrap*	array_CL4P[4];
+	{
+		array_CL4P[0] = new ClapTrap("Base");
+		array_CL4P[1] = new ScavTrap("Scav");
+		array_CL4P[2] = new FragTrap("Frag");
+		array_CL4P[3] = new DiamondTrap("Diams");
+	}
+
+	{
+		for (int i = 0; i < 4; i++) {
+			array_CL4P[i]->displayStats();
+			array_CL4P[i]->attack("bob");
+		}
+	}
+
+	{
+		for (int i = 0; i < 4; i++) {
+			delete array_CL4P[i];
+		}
+	}
+
 }
