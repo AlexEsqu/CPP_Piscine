87d99a03a7d3e22c0f1aff7708823e606c8ec932
Alex <momiji.kling@gmail.com>
2025-05-25 15:11:50 +0200

    Added logging info to Template ex02

--- CPP_07_TEMPLATE/ex02/inc/Array.hpp
+++ CPP_07_TEMPLATE/ex02/inc/Array.hpp
@@ -2,6 +2,26 @@
 # define CLASS_ARRAY_H
 
 # include <stdexcept>
+# include <iostream>
+
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define BLUE "\033[34m"
+# define MAGENTA "\033[35m"
+# define CYAN "\033[36m"
+# define STOP_COLOR "\033[0m"
+
+# define FAINT "\033[2m"
+# define BOLD "\033[1m"
+# define STOP_STYLE "\033[22m"
+
+# define ITALIC "\033[3m"
+# define STOP_ITALIC "\033[23m"
+
+# ifndef DEBUG
+#  define DEBUG 0
+# endif
 
 template<typename T>
 class Array {
@@ -16,6 +36,11 @@ class Array {
 		Array(unsigned int n = 0)
 			: _size(n)
 		{
+			if (DEBUG) {
+				std::cout << GREEN << "Constructing an Array of size ";
+				std::cout << _size << STOP_COLOR << std::endl;
+			}
+
 			if (_size > 0)
 				_content = new T[_size];
 			else
@@ -29,6 +54,11 @@ class Array {
 		Array(const Array& original)
 			: _size(original._size)
 		{
+			if (DEBUG) {
+				std::cout << GREEN << "Copy constructing an Array of size ";
+				std::cout << _size << STOP_COLOR << std::endl;
+			}
+
 			if (_size > 0)
 				_content = new T[_size];
 			else
@@ -40,11 +70,21 @@ class Array {
 		}
 
 		~Array() {
+			if (DEBUG) {
+				std::cout << RED << "Destructing an Array of size ";
+				std::cout << _size << STOP_COLOR << std::endl;
+			}
+
 			delete[] _content;
 		}
 
 		Array&			operator=(const Array& original)
 		{
+			if (DEBUG) {
+				std::cout << GREEN << "Copy assigning an Array of size ";
+				std::cout << original._size << STOP_COLOR << std::endl;
+			}
+
 			if (this != original) {
 				delete[] _content;
 				_size = original._size;
