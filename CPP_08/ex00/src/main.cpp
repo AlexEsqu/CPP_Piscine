47ac33f0978e19e1844c53e12b94d14a7da237c9
Alex <momiji.kling@gmail.com>
2025-05-25 19:22:54 +0200

    added list test in CPP08 ex00

--- CPP_08/ex00/src/main.cpp
+++ CPP_08/ex00/src/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <list>
 #include "easyfind.hpp"
 
-#define VECTOR_LENGTH 10
+#define TEST_LENGTH 10
 
 int	main()
 {
 	std::vector<int>	intVector;
 
-	for (int i = 0; i < VECTOR_LENGTH; i++) {
+	for (int i = 0; i < TEST_LENGTH; i++) {
 		intVector.push_back(i + 10);
 	}
 
@@ -24,6 +25,27 @@ int	main()
 	}
 	catch (const std::out_of_range& e) {
 		std::cout << "Since it doesn't exist in the vector, ";
+		std::cout << "0 cannot be found: " << e.what() << std::endl;
+	}
+
+	std::list<int>	intList;
+
+	for (int i = 0; i < TEST_LENGTH; i++) {
+		intList.push_back(i + 10);
+	}
+
+	std::list<int>::iterator	other_result;
+	other_result = easyfind(intList, 12);
+	std::cout << *other_result << " was found at position ";
+	std::cout << std::distance(intList.begin(), other_result) << std::endl;
+
+	try {
+		other_result = easyfind(intList, 0);
+		std::cout << *other_result << " was found at position ";
+		std::cout << std::distance(intList.begin(), other_result) << std::endl;
+	}
+	catch (const std::out_of_range& e) {
+		std::cout << "Since it doesn't exist in the List, ";
 		std::cout << "0 cannot be found: " << e.what() << std::endl;
 	}
 
