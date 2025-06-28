096bd1a93d024f1deb4b434f5a9a2ee74a65e12f
Alex Kling <momiji.kling@gmail.com>
2025-06-28 10:12:01 +0200

    Procrastinating the sort again

--- CPP_09/ex02/src/PmergeMe.cpp
+++ CPP_09/ex02/src/PmergeMe.cpp
@@ -1,14 +1,29 @@
 #include "PmergeMe.hpp"
 
 
+void	groupElementsByPairs(std::vector<int> intVector, bool hasStraggler)
+{
+
+}
+
+
+
+
+
+
+
+
 
 
 
 void	MergeInsertSort(std::vector<int> intVector)
 {
+	bool hasStraggler = (intVector.size() % 2 != 0);
+
+	
+
 	//Group into n/2 pair of elements and set straggler
 	//perform n/2 comparison to determine bigger in each pair
 	//recursively sort n/2 larger element from each pair into sorted S sequence
 	//insert start of S the element paired with first and smallest element of S
-
 }
