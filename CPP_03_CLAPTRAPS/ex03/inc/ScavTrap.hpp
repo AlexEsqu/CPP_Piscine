528d8bee6e36c1eb87fa35127975d273d35b8823
Alex Kling <m2kling@gmail.com>
2025-09-26 16:42:02 +0200

    Diamond inheritance slightly cooler with only a virtual on the initial attack()

--- CPP_03_CLAPTRAPS/ex03/inc/ScavTrap.hpp
+++ CPP_03_CLAPTRAPS/ex03/inc/ScavTrap.hpp
@@ -27,7 +27,7 @@ class ScavTrap: virtual public ClapTrap {
 	void			guardGate();
 
 	// ---------------- INHERITED ------------------------//
-	virtual void	attack( const std::string& target );
+	void			attack( const std::string& target );
 	// void			takeDamage( unsigned int amount );
 	// void			beRepaired( unsigned int amount );
 
